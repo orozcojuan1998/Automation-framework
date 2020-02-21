1ccc9fa964aa738c5476522dcf8ec8f440481bdd
Diana Ardila <diana.ardila@endava.com>
2020-02-21 11:52:16 -0500

    Log in UI

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,13 +1,65 @@
-public class LogIn {
-    public LogIn() {
-        Given("^As an user I want to be able to log in on Facebook with email$", () -> {
-        });
-        When("^The user enters his valid email and password$", () -> {
-        });
-        And("^clicks on the Login button$", () -> {
-        });
-        Then("^the user is redirected to Facebook's home page$", () -> {
-        });
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
+public class LogIn {
+    @Given("^As an user I want to be able to log in on Facebook with email$")
+    public void asAnUserIWantToBeAbleToLogInOnFacebookWithEmail() {
+    }
+
+    @When("^The user enters his valid email and password$")
+    public void theUserEntersHisValidEmailAndPassword() {
+    }
+
+    @And("^clicks on the Login button$")
+    public void clicksOnTheLoginButton() {
+    }
+
+    @Then("^the user is redirected to Facebook's home page$")
+    public void theUserIsRedirectedToFacebookSHomePage() {
+    }
+
+    @Given("^User is in Facebook's main page$")
+    public void userIsInFacebookSMainPage() {
+    }
+
+    @When("^The user enters his valid phone number and password$")
+    public void theUserEntersHisValidPhoneNumberAndPassword() {
+    }
+
+    @When("^The user enters his valid email and invalid password$")
+    public void theUserEntersHisValidEmailAndInvalidPassword() {
+    }
+
+    @Then("^the user receives a message \"([^\"]*)\"$")
+    public void theUserReceivesAMessage(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^The user leaves the fields in blank$")
+    public void theUserLeavesTheFieldsInBlank() {
+    }
+
+    @When("^The user clicks on \"([^\"]*)\" message$")
+    public void theUserClicksOnMessage(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^The user enters his valid email$")
+    public void theUserEntersHisValidEmail() {
+    }
+
+    @And("^clicks on \"([^\"]*)\"$")
+    public void clicksOn(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^user receives a confirmation email$")
+    public void userReceivesAConfirmationEmail() {
     }
 }
--- src/test/java/LoginCellphone.java
+++ src/test/java/LoginCellphone.java
@@ -1,22 +1,5 @@
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 
-public class LoginCellphone {
-    @Given("^User is on Facebook app$")
-    public void userIsOnFacebookApp() {
-    }
 
-    @When("^enters his valid email and password$")
-    public void entersHisValidEmailAndPassword() {
-    }
 
-    @And("^tap on the login button$")
-    public void tapOnTheLoginButton() {
-    }
 
-    @Then("^the user is redirected to Facebook's app home$")
-    public void theUserIsRedirectedToFacebookSAppHome() {
-    }
-}
+
